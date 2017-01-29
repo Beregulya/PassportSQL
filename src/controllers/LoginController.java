d45b451bcc5568bbc740e5c5f6d511f06cfc1154
Beregulya <beregulya.dev@gmail.com>
2017-01-29 22:20:30 +0200

    Initial commit

---
+++ src/controllers/LoginController.java
@@ -0,0 +1,25 @@
+package controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class LoginController {
+
+    @FXML private TextField nameField;
+    @FXML private PasswordField passField;
+    @FXML private Button logInButton;
+    @FXML private Button signUpButton;
+
+    @FXML
+    public void onActionLogInButton() {
+
+    }
+
+    @FXML
+    public void onActionSignUpButton() {
+
+    }
+
+}
